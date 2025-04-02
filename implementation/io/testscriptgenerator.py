62e7d6fdd89616fc7b7e516e284a01b5ff1cf50a
SIeepii <pyao477@gmail.com>
2025-04-02 10:20:37 -0400

    updated script

--- implementation/io/testscriptgenerator.py
+++ implementation/io/testscriptgenerator.py
@@ -1,6 +1,13 @@
 for i in range(20):
     try:
         f = open(f"test.in.{i}", "x")
+        f.close()
+        print("File created successfully.")
+    except FileExistsError:
+        print("File already exists.")
+
+for i in range(20):
+    try:
         f = open(f"test.out.{i}", "x")
         f.close()
         print("File created successfully.")
