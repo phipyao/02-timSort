6569157ee34b987b74307b0969d1da857d4d5247
SIeepii <pyao477@gmail.com>
2025-04-02 10:19:24 -0400

    added files

---
+++ implementation/io/testscriptgenerator.py
@@ -0,0 +1,8 @@
+for i in range(20):
+    try:
+        f = open(f"test.in.{i}", "x")
+        f = open(f"test.out.{i}", "x")
+        f.close()
+        print("File created successfully.")
+    except FileExistsError:
+        print("File already exists.")
