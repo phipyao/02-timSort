c9d5f0ed4b3e2f7ef598879b9f6078339897cb16
SIeepii <pyao477@gmail.com>
2025-04-18 18:32:18 -0400

    worked on pcSol

---
+++ programmingChallenge/solutions/pcSol_python.py
@@ -0,0 +1,172 @@
+from functools import cmp_to_key
+
+def calculate_min_run(n):
+    r = 0
+    while n >= 32:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+def insertion_sort(arr, left, right, key):
+    if left < right and key(arr[left]) > key(arr[left+1]):
+        is_desc = True
+        for k in range(left, right):
+            if key(arr[k]) < key(arr[k+1]):
+                is_desc = False
+                break
+        if is_desc:
+            i, j = left, right
+            while i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+            return
+
+    for i in range(left + 1, right + 1):
+        temp = arr[i]
+        j = i - 1
+        while j >= left and key(arr[j]) > key(temp):
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+
+
+def merge(arr, left, mid, right, key):
+    left_part = arr[left:mid + 1]
+    right_part = arr[mid + 1:right + 1]
+
+    i = j = 0
+    k = left
+    while i < len(left_part) and j < len(right_part):
+        if key(left_part[i]) <= key(right_part[j]):
+            arr[k] = left_part[i]
+            i += 1
+        else:
+            arr[k] = right_part[j]
+            j += 1
+        k += 1
+
+    while i < len(left_part):
+        arr[k] = left_part[i]
+        i += 1
+        k += 1
+
+    while j < len(right_part):
+        arr[k] = right_part[j]
+        j += 1
+        k += 1
+
+
+def timsort(arr, key=lambda x: x):
+    n = len(arr)
+    min_run = calculate_min_run(n)
+    run_stack = []
+
+    i = 0
+    while i < n:
+        run_end = min(i + min_run - 1, n - 1)
+        insertion_sort(arr, i, run_end, key)
+        run_stack.append((i, run_end))
+        i = run_end + 1
+
+        merge_collapse(arr, run_stack, key)
+
+    while len(run_stack) > 1:
+        merge_at(arr, run_stack, len(run_stack) - 2, key)
+
+
+def merge_collapse(arr, run_stack, key):
+    while len(run_stack) > 2:
+        A = run_stack[-3]
+        B = run_stack[-2]
+        C = run_stack[-1]
+        lenA = A[1] - A[0] + 1
+        lenB = B[1] - B[0] + 1
+        lenC = C[1] - C[0] + 1
+
+        if lenA <= lenB + lenC or lenB <= lenC:
+            if lenA < lenC:
+                merge_at(arr, run_stack, len(run_stack) - 3, key)
+            else:
+                merge_at(arr, run_stack, len(run_stack) - 2, key)
+        else:
+            break
+
+    if len(run_stack) == 2 and (run_stack[-2][1] - run_stack[-2][0] + 1) <= (run_stack[-1][1] - run_stack[-1][0] + 1):
+        merge_at(arr, run_stack, len(run_stack) - 2, key)
+
+
+def merge_at(arr, run_stack, i, key):
+    start1, end1 = run_stack[i]
+    start2, end2 = run_stack[i + 1]
+
+    merge(arr, start1, end1, end2, key)
+    run_stack[i] = (start1, end2)
+    del run_stack[i + 1]
+
+def polar_angle_comparator(a, b):
+    ax, ay, ra, ia = a
+    bx, by, rb, ib = b
+
+    # Check Above/Below Lighthouse
+    def half(x, y):
+        return y > 0 or (y == 0 and x >= 0)
+
+    ha = half(ax, ay)
+    hb = half(bx, by)
+
+    if ha != hb:
+        return -1 if ha else 1
+
+    # Cross product for angle
+    cross = ax * by - ay * bx
+    if cross != 0:
+        return -1 if cross > 0 else 1
+
+    # Sort by radius
+    if ra != rb:
+        return -1 if ra < rb else 1
+
+    # Sort by order of appearance (i)
+    if ia != ib:
+        return -1 if ia < ib else 1
+
+    return 0
+
+def radius_comparator(a, b):
+    ax, ay, ra, ia = a
+    bx, by, rb, ib = b
+
+    # Sort by radius
+    if ra != rb:
+        return -1 if ra < rb else 1
+
+    # Sort by order of appearance (i)
+    if ia != ib:
+        return -1 if ia < ib else 1
+
+    return 0
+
+def query_points_within_radius(points, query_r):
+    # Binary Search
+    low, high = 0, len(points)
+    while low < high:
+        mid = (low + high) // 2
+        if points[mid][2] <= query_r:
+            low = mid + 1
+        else:
+            high = mid
+    return points[:low]
+
+# main:
+# arr_size = int(input())
+# arr = [int(input()) for i in range(arr_size)]
+# timsort(arr)
+# print(*arr)
+
+# TODO: calculate radius for every point and append index (x, y) -> (x, y, r, i)
+# TODO: make a hashmap with a key of points "(x, y, r, i)" and value of "boatname"
+# TODO: timsort points by radius r, i
+# TODO: query subarray of points by binary searching all points under given radius
+# TODO: timsort subarray by polar angle -> sorted_points
+# TODO: get values from hashmap of sorted_points and print boat names
